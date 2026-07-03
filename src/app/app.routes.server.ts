83418f12ea5279e43cbdcc55f363d707263e41a5
Christian Oliver <obiekrix@gmail.com>
2026-07-03 23:34:04 +0100

    Update server routes: add distinct paths for "teacher" and "student" with client-side rendering; adjust fallback to render in client mode

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'teacher',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'student',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Client
   }
 ];
